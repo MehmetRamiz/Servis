de1c55b00d49b1ba530a857cef57728ef287371d
MehmetRamiz <mehmedramiztas@gmail.com>
2021-10-02 22:42:33 +0300

    added new class

---
+++ source/repos/ServisProgramı.API/ServisProgrami.Core/Models/Iscilik.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServisProgrami.Core.Models
+{
+    class Iscilik
+    {
+        public int id { get; set; }
+
+        public int OnarimNo { get; set; }
+
+        public string OnarimAdi { get; set; }
+
+        public int Miktar { get; set; }
+
+        public int birim { get; set; }
+
+        public int Tutar { get; set; }
+    }
+}
